50bd40d1d2071115596d2dc56c9f2a561912f702
joao bittencourt <joaogsbittencourt@gmail.com>
2022-12-28 23:56:07 -0300

    first commit

---
+++ tests/ConfigTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace src\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase {
+    
+     public function test_cofig() {
+          $this->assertEquals(1,1);
+     }
+}
