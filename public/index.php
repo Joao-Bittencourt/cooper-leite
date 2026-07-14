4dbb31345f3577b0d3433d1121d3300720398175
Joao Bittencourt <joaogsbittencourt@gmail.com>
2026-07-13 23:11:51 -0300

    Open telemetry

--- public/index.php
+++ public/index.php
@@ -12,4 +12,27 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/core/basics.php';
 require_once __DIR__ . '/../src/routes.php';
 
-$router->run($router->routes);
+if (class_exists(\OpenTelemetry\API\Globals::class)) {
+    $propagator = \OpenTelemetry\API\Trace\Propagation\TraceContextPropagator::getInstance();
+    $context = $propagator->extract($_SERVER);
+
+    $tracer = \OpenTelemetry\API\Globals::tracerProvider()->getTracer('cooper-leite');
+    $span = $tracer->spanBuilder($_SERVER['REQUEST_METHOD'] . ' ' . ($_SERVER['REQUEST_URI'] ?? '/'))
+        ->setParent($context)
+        ->setSpanKind(\OpenTelemetry\API\Trace\SpanKind::KIND_SERVER)
+        ->startSpan();
+    $spanScope = $span->activate();
+
+    try {
+        $router->run($router->routes);
+    } catch (\Throwable $e) {
+        $span->recordException($e);
+        $span->setStatus(\OpenTelemetry\API\Trace\StatusCode::STATUS_ERROR, $e->getMessage());
+        throw $e;
+    } finally {
+        $span->end();
+        $spanScope->detach();
+    }
+} else {
+    $router->run($router->routes);
+}
